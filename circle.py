248f10bc48358bf5a4c4ab876cdae0004bb5ed99
Komperlan <legionerjustin@gmail.com>
2023-10-11 22:33:06 +0300

    add documentation

--- circle.py
+++ circle.py
@@ -1,10 +1,35 @@
 import math
-
+"Импортируем библиотеку math для работы с числом пи"
 
 def area(r):
+    """
+    Площадь круга
+
+        Параметры:
+            :param: r(int, float, double) радиус круга
+
+        Возврощаемое значение
+            :return: Pi * (r**2)  (float, double)Площадь круга
+
+    Пример вызова:
+        input(3)
+        output(28.274333882308138)
+    """
     return math.pi * r * r
 
-
 def perimeter(r):
+    """
+    Длина окружности
+
+        Параметры:
+            :param: r(int, float, double) радиус круга
+
+        Возврощаемое значение
+            :return: 2 * Pi * r (float, double) Площадь круга
+
+    Пример вызова:
+        input(2)
+        output(12.566370614359172)
+    """
     return 2 * math.pi * r
 
--- triangle.py
+++ triangle.py
@@ -1,5 +1,34 @@
 def area(a, h):
+    """
+    Получает площадь треугольника по стороне и высоте
+
+    Параметры
+        :param a: сторона  (int, float)
+        :param h: высота (int, float)
+    Возврпщает
+        :return: a * h / 2 Площадь(половина произведения a на h) (int, float)
+
+
+    Пример:
+        a = 6
+        h = 4
+        :return 12
+    """
     return a * h / 2
 
 def perimeter(a, b, c):
-    return (a + b + c)*2
+    """
+    Возвращает периметр треугольника по его сторонам
+
+    Параметры
+        :param a: сторона 1 (int, float)
+        :param b: сторона 2 (int, float)
+        :param c: сторона 3 (int, float)
+    Возврощает
+        :return: a + b + c перимитер сумма a, b, c  (int, float)
+
+    Пример:
+        input (1, 2, 3)
+        output(6)
+    """
+    return a + b + c
